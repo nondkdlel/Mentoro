a567875d5fde522b59a23d6d9040e2e71d584142
nondkdlel <nondkdlel@naver.com>
2024-12-29 04:28:46 +0900

    setting update

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+      // "@comp/*": ["./src/components/*"],
+      // "@route/*": ["./src/routes"],
+      // "@layout/*": ["./src/routes/layouts/*"],
+      // "@page/*": ["./src/routes/pages/*"],
+      // "@style/*": ["./src/styles/*"],
+      // "@valtio/*": ["./valtio/*"]
+    ],
+  },
 });
