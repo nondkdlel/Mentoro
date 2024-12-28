00e12e00316b7a14337d1c0374cba0045c5a2045
nondkdlel <nondkdlel@naver.com>
2024-12-29 07:37:33 +0900

    test build

--- valtio/alarm/AlarmStatus.ts
+++
@@ -1,8 +0,0 @@
-import { proxy } from "valtio";
-
-export type AlarmModalStatusType = {
-  status: boolean;
-};
-export const proxyAlarmModalStatus = proxy<AlarmModalStatusType>({
-  status: false,
-});
--- valtio/common/ActiveTab.ts
+++
@@ -1,9 +0,0 @@
-import { proxy } from "valtio";
-
-interface ActiveTabType {
-  tabId: number;
-}
-
-export const proxyActiveTab = proxy<ActiveTabType>({
-  tabId: 0,
-});
